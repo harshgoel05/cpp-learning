b5799db66414649e2b3dd54a3f95770a3fd73149
harshgoel05 <harshgoel05@gmail.com>
2021-03-09 20:22:03 +0530

    ⚡️ Add day 1

---
+++ 01 Introduction/Introduction.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+main()
+{
+    char name[200];
+    cout << "Input your name: ";
+    cin >> name;
+    cout << "Let's get started with C++ " << name;
+    ;
+}
+
+//Output
+
+// Input your name: Harsh
+// Let's get started with C++ Harsh

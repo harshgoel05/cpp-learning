8ff8388bf1810ef3727867ca756bbacc17590219
harshgoel05 <harshgoel05@gmail.com>
2021-06-18 20:46:33 +0530

    ⚡️ Add optimal rorate solution

--- 01 Arrays/Rotate an Array.cpp
+++ 01 Arrays/Rotate an Array.cpp
@@ -1,3 +1,51 @@
+// GFG Solution:
+// C++ program to rotate an array by
+// d elements
+
+
+//  Can use temp variable (optimal), temp array 
+#include <iostream>
+using namespace std;
+ 
+/*Fuction to get gcd of a and b*/
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+ 
+    else
+        return gcd(b, a % b);
+}
+ 
+/*Function to left rotate arr[] of siz n by d*/
+void leftRotate(int arr[], int d, int n)
+{
+    /* To handle if d >= n */
+    d = d % n;
+    int g_c_d = gcd(d, n);
+    for (int i = 0; i < g_c_d; i++) {
+        /* move i-th values of blocks */
+        int temp = arr[i];
+        int j = i;
+ 
+        while (1) {
+            int k = j + d;
+            if (k >= n)
+                k = k - n;
+ 
+            if (k == i)
+                break;
+ 
+            arr[j] = arr[k];
+            j = k;
+        }
+        arr[j] = temp;
+    }
+}
+ 
+
+// My solution:
+
 void rotateArr(int arr[], int d, int n){
     int temp[d];
     int k =0;
